1208fd1774a9c78ab10e8fd27085c8fec8440762
nicolau-pungue <162338064+nicolau-pungue@users.noreply.github.com>
2024-03-06 12:54:50 +0000

    trabalhando na criacao do template e controllers

---
+++ app/controllers/Actividades.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Actividades extends Controller {
+
+    public static void index(){
+
+    }
+
+    public static void create(){
+
+    }
+
+    public static void store(){
+
+    }
+}
