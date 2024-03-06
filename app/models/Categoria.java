392ca07c293a058339e84eb10e07320efa830ca8
nicolau-pungue <162338064+nicolau-pungue@users.noreply.github.com>
2024-03-06 16:40:32 +0000

    trabalhando no relacionamento de modelo

--- app/models/Categoria.java
+++ app/models/Categoria.java
@@ -3,10 +3,15 @@ package models;
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Categoria extends Model {
     private String categoria;
+
+    @OneToMany
+    private List<Categoria> categorias;
 
     public Categoria(String categoria) {
         this.categoria = categoria;
